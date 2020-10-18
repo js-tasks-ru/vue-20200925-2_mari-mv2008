b91c60315dab396b63f35188bb7a7946ea4ceecd
MariyaBV <mari.mv2008@gmail.com>
2020-10-18 16:02:21 +0300

    solved problem 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,25 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" v-on:click="increment">{{count}}</button>',
 
   // Компонент должен иметь входной параметр
-
   // Компонент должен иметь модель
-
+  model: {
+    prop: 'count',
+    event: 'increment'
+  },
+  props: {
+    count: {
+      name: 'Numeric',
+      type: Number,
+      required: false,
+      default: 0
+    } 
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    increment() {
+        return this.$emit('increment', this.count + 1);
+    }
+  }
 };
